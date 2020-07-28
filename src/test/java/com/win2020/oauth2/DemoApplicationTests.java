3e5aba014f8372d6b24f2af66e9a6a8ba54545a2
Lisa Miller <lisayevonne@outlook.com>
2020-07-28 08:15:00 -0400

    first commit

---
+++ src/test/java/com/win2020/oauth2/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.win2020.oauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
